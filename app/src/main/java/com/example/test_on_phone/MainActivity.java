7703081289c85a35ed20c08805439a0d83a53b37
ZhangAoCanada <zhangaocanada@gmail.com>
2020-02-24 12:56:23 -0500

    example of broadcasting does not work well.

--- app/src/main/java/com/example/test_on_phone/MainActivity.java
+++ app/src/main/java/com/example/test_on_phone/MainActivity.java
@@ -2,33 +2,48 @@ package com.example.test_on_phone;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
-
 import android.app.Activity;
-import android.util.Log;
+import android.content.Intent;
+//import android.util.Log;
 import android.view.View;
 
 //public class MainActivity extends AppCompatActivity {
 public class MainActivity extends Activity {
-    String msg = "Android : ";
 
-    /** Called when the activity is first created. */
+    /** Example for android broadcasting */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(msg, "The onCreate() event.");
     }
 
-	public void startService(View view) {
-		startService(new Intent(getBaseContext(), MyService.class));
-	}
+    // broadcast a custom intent.
+    public void broadcastIntent(View view) {
+        Intent intent = new Intent();
+        intent.setAction("HelloWorld"); sendBroadcast(intent);
+    }
 
-	//Method to stop the service
-	public void stopService(View view) {
-		stopService(new Intent(getBaseContext(), MyService.class));
-	}
+    /** Example for android service */
+//    String msg = "Android : ";
+//
+//    /** Called when the activity is first created. */
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        Log.d(msg, "The onCreate() event.");
+//    }
+//
+//	public void startService(View view) {
+//		startService(new Intent(getBaseContext(), MyService.class));
+//	}
+//
+//	//Method to stop the service
+//	public void stopService(View view) {
+//		stopService(new Intent(getBaseContext(), MyService.class));
+//	}
 
+	/** Example for the android activities */
 //    /** Called when the activity is about to become visible. */
 //    @Override
 //    protected void onStart() {
