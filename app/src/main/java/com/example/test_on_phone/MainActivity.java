48fcb9d9f1489a13d88485b3120bc495838daf2b
ZhangAoCanada <zhangaocanada@gmail.com>
2020-02-26 14:54:59 -0500

    camera api preview is not working, try new things.

--- app/src/main/java/com/example/test_on_phone/MainActivity.java
+++ app/src/main/java/com/example/test_on_phone/MainActivity.java
@@ -1,57 +1,62 @@
 package com.example.test_on_phone;
 
-import android.net.Uri;
+import android.hardware.Camera;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 
-import android.content.ContentValues;
-import android.content.CursorLoader;
+import android.content.Context;
+import android.content.pm.PackageManager;
 
-import android.database.Cursor;
-
-import android.view.Menu;
 import android.view.View;
 
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.FrameLayout;
+
+import android.media.MediaRecorder;
+import android.media.MediaPlayer;
+
+import java.io.IOException;
 
 public class MainActivity extends Activity {
+    private Camera mCamera;
+    private CameraPreview mPreview;
+    public boolean cam_status;
+
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    public void onClickAddName(View view) {
-        // Add a new student record
-        ContentValues values = new ContentValues();
-        values.put(StudentsProvider.NAME,
-                ((EditText)findViewById(R.id.editText2)).getText().toString());
 
-        values.put(StudentsProvider.GRADE,
-                ((EditText)findViewById(R.id.editText3)).getText().toString());
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
 
-        Uri uri = getContentResolver().insert(
-                StudentsProvider.CONTENT_URI, values);
-
-        Toast.makeText(getBaseContext(),
-                uri.toString(), Toast.LENGTH_LONG).show();
-    }
-    public void onClickRetrieveStudents(View view) {
-        // Retrieve student records
-        String URL = "content://com.example.MyApplication.StudentsProvider";
-
-        Uri students = Uri.parse(URL);
-        Cursor c = managedQuery(students, null, null, null, "name");
-
-        if (c.moveToFirst()) {
-            do{
-                Toast.makeText(this,
-                        c.getString(c.getColumnIndex(StudentsProvider._ID)) +
-                                ", " +  c.getString(c.getColumnIndex( StudentsProvider.NAME)) +
-                                ", " + c.getString(c.getColumnIndex( StudentsProvider.GRADE)),
-                        Toast.LENGTH_SHORT).show();
-            } while (c.moveToNext());
-        }
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
     }
 }
