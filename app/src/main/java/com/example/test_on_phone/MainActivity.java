faeddfd73a2e6a182648fb79fcaafa5a39e33244
ZhangAoCanada <zhangaocanada@gmail.com>
2020-02-26 13:43:44 -0500

    update to the content provider.

--- app/src/main/java/com/example/test_on_phone/MainActivity.java
+++ app/src/main/java/com/example/test_on_phone/MainActivity.java
@@ -1,82 +1,57 @@
 package com.example.test_on_phone;
 
+import android.net.Uri;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
-import android.content.Intent;
-//import android.util.Log;
+
+import android.content.ContentValues;
+import android.content.CursorLoader;
+
+import android.database.Cursor;
+
+import android.view.Menu;
 import android.view.View;
 
-//public class MainActivity extends AppCompatActivity {
+import android.widget.EditText;
+import android.widget.Toast;
+
 public class MainActivity extends Activity {
 
-    /** Example for android broadcasting */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void onClickAddName(View view) {
+        // Add a new student record
+        ContentValues values = new ContentValues();
+        values.put(StudentsProvider.NAME,
+                ((EditText)findViewById(R.id.editText2)).getText().toString());
 
-    // broadcast a custom intent.
-    public void broadcastIntent(View view) {
-        Intent intent = new Intent();
-        intent.setAction("HelloWorld"); sendBroadcast(intent);
+        values.put(StudentsProvider.GRADE,
+                ((EditText)findViewById(R.id.editText3)).getText().toString());
+
+        Uri uri = getContentResolver().insert(
+                StudentsProvider.CONTENT_URI, values);
+
+        Toast.makeText(getBaseContext(),
+                uri.toString(), Toast.LENGTH_LONG).show();
     }
+    public void onClickRetrieveStudents(View view) {
+        // Retrieve student records
+        String URL = "content://com.example.MyApplication.StudentsProvider";
 
-    /** Example for android service */
-//    String msg = "Android : ";
-//
-//    /** Called when the activity is first created. */
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//        Log.d(msg, "The onCreate() event.");
-//    }
-//
-//	public void startService(View view) {
-//		startService(new Intent(getBaseContext(), MyService.class));
-//	}
-//
-//	//Method to stop the service
-//	public void stopService(View view) {
-//		stopService(new Intent(getBaseContext(), MyService.class));
-//	}
+        Uri students = Uri.parse(URL);
+        Cursor c = managedQuery(students, null, null, null, "name");
 
-	/** Example for the android activities */
-//    /** Called when the activity is about to become visible. */
-//    @Override
-//    protected void onStart() {
-//        super.onStart();
-//        Log.d(msg, "The onStart() event.");
-//    }
-//
-//    /** Called when the activity has become visible. */
-//    @Override
-//    protected void onResume() {
-//        super.onResume();
-//        Log.d(msg, "The onResume() event.");
-//    }
-//
-//    /** Called when another activity is taking focus. */
-//    @Override
-//    protected void onPause() {
-//        super.onPause();
-//        Log.d(msg, "The onPause() event.");
-//    }
-//
-//    /** Called when the activity is no longer visible. */
-//    @Override
-//    protected void onStop() {
-//        super.onStop();
-//        Log.d(msg, "The onStop() event.");
-//    }
-//
-//    /** Called just before the activity is destroyed. */
-//    @Override
-//    protected void onDestroy() {
-//        super.onDestroy();
-//        Log.d(msg, "The onDestroy() event.");
-//    }
+        if (c.moveToFirst()) {
+            do{
+                Toast.makeText(this,
+                        c.getString(c.getColumnIndex(StudentsProvider._ID)) +
+                                ", " +  c.getString(c.getColumnIndex( StudentsProvider.NAME)) +
+                                ", " + c.getString(c.getColumnIndex( StudentsProvider.GRADE)),
+                        Toast.LENGTH_SHORT).show();
+            } while (c.moveToNext());
+        }
+    }
 }
-
